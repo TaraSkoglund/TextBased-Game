06f5bb35486a2bd68e4b5033a0a2a624ac81aafc
Tara Skoglund <skoglundtara@gmail.com>
2022-11-20 17:03:54 +0100

    Start Over base

--- main.js
+++ main.js
@@ -1,60 +1,38 @@
-window.addEventListener("DOMContentLoaded", main); 
+window.addEventListener("DOMContentLoaded", main);
+
+let textStart;
+let text1;
+let Start;
+let firstStage;
+let input;
+let button1;
+let button2;
+let button3;
+let savedInput;
+let label;
+let startOver;
+let textStartOver;
+let buttonStartOver
 
 function main() {
-    loadHtmlElements
+    loadHtmlElements()
+    loadStartStage()
 }
 
 function loadHtmlElements() {
-    let story = 0;
-    let form = document.getElementById('theAdventure');
-    let input = document.getElementById('name-input');
-    let button1 = document.getElementById('opt-1');
-    let button2 = document.getElementById('opt-2');
-    let answer = '';
-}
-
-let story_telling = {
-    "start":{
-        "question": "Aslan needs your help to retrieve his almighty nail file, he needs it to win a war he has been fighting for over thousand years, tight claws. Do you accept this important assignment?",
-        "answers":{
-            "a": "I accept this most important assignment.",
-            "b": "I'd rather go back into the closet."
-        }
-    },
-
-    /** I Accept Path */
-    "1_a":{
-        "question": "Asland informs you that he has traced the almighty nail file to a village of black elves, and that he is unable to retrieve it himself as his paws hurt too much. He gives you the choice of taking his mighty sword or a mage's bag withe you.",
-        "answers":{
-            "a": "I name choose to take with me the mighty sword",
-            "b": "I name choose to take the magician's bag with me"
-        }
-    },
-
-    /** To The Closet Path */
-    "1_b":{
-        "question": "You will be well rewarded for your heroism",
-        "answers":{
-            "a": "Okey I accept the assignment.",
-            "b": "Aslan bites off your head for your disrespectfulness."
-        }
-    }
-};
-
-/** Reset Button */
-function resetForm(){
-    document.getElementById("theAdventure").reset();
-}
-
-/** Generate answers from story */
-function populateForm(story) {
-    let current_story = story_telling[story];
-    let text = '';
-    for (let prop in current_story['answers']){
-        if (current_story['answers'].hasOwnProperty(prop)){
-            text += '<label><input name="answer" value="' + prop + '"/><span' + current_story['answers'][prop] + '</span></label>';
-        }
-    }
+    textStart = document.getElementById('textStart');
+    text1 = document.getElementById('text-1');
+    Start = document.getElementById('startStage');
+    firstStage = document.getElementById('theAdventure');
+    input = document.getElementById('name-input');
+    button1 = document.getElementById('opt-Start');
+    button2 = document.getElementById('opt-1');
+    button3 = document.getElementById('opt-2');
+    savedInput = document.getElementById('input'.value);
+    label = document.getElementById('label-text');
+    startOver = document.getElementById('startOver');
+    textStartOver = document.getElementById('text-startOver');
+    buttonStartOver = document.getElementById('opt-startOver');
 }
 
 
