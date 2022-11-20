1dae07c77c7e2ab73af4f3dbb1dd52230f5772bd
Tara Skoglund <skoglundtara@gmail.com>
2022-11-20 21:11:35 +0100

    story telling done

--- main.js
+++ main.js
@@ -83,4 +83,74 @@ function loadRatherStage(){
 
     button3.textContent = "Aslan bites off your head for your disrespectfulness."
     button3.addEventListener('click',returnToStart);
+}   
+    /** third Scene 1a*/
+function loadVillageStage(){
+    text1.textContent = "When you arrive in the black elfs village you realize that it is the king of black elfs who stole the nail file, what will you do to seek him out?"
+
+    button2.textContent = "I will seek an audience with him and ask him to return Aslans nail file"
+    button2.addEventListener('click',loadAudienceStage);
+
+    button3.textContent = "I will break into the throne room and force him to hand over Aslans nail file"
+    button3.addEventListener('click',loadFightStage);
+}
+/** Fourth Scene 1a */
+function loadAudienceStage(){
+    text1.textContent = "The black elvis king grants your request for an audience with him and he informs you that there are only two ways for you to retrieve the almighty nail file."
+
+    button2.textContent = "By fighting to the death."
+    button2.addEventListener('click',loadFightStage);
+
+    button3.textContent = "Or to exchange it for something equally viluable."
+    button3.addEventListener('click',loadExchangeStageBag);
+}
+/** Third Scene 2a */
+  function returnToStart(){
+      startOver.classList.remove("hidden");
+      firstStage.classList.add("hidden");
+
+      textStartOver.textContent = "Your head was cut off"
+
+      buttonStartOver.textContent = "Start over"
+      buttonStartOver.addEventListener('click',loadStartStage);
+}
+/** Fourth Scene 2a */
+function loadFightStage(){
+    startOver.classList.remove("hidden");
+    firstStage.classList.add("hidden");
+
+    textStartOver.textContent = "A battle breaks out and you have the upper hand, but the king is cunning and cannot be trusted. and before you know it you are on the ground with an arrow through the chest dying."
+
+    buttonStartOver.textContent = "Start over"
+    buttonStartOver.addEventListener('click',loadStartStage);
+}
+/** Fainel Scene whith the bag */
+function loadExchangeStageBag(){
+    
+    firstStage.classList.remove("hidden");
+    Start.classList.add("hidden");
+    startOver.classList.add("hidden"); 
+
+    text1.textContent = "You open the magic bag and sees an exact copy of Aslans nail file and tells the king why steal someone else's when you can have your own. The king agrees to the exchange and Aslan rewards you for a job well done."
+
+    button2.textContent = "Congratulations."
+    button2.addEventListener('click',loadStartStage);
+
+    button3.textContent = "Start Over."
+    button3.addEventListener('click',loadStartStage);
+}
+/** Fainel Scene whith the sword */
+function loadExchangeStageSword(){
+
+    firstStage.classList.remove("hidden");
+    Start.classList.add("hidden");
+    startOver.classList.add("hidden"); 
+
+    text1.textContent = "You ask if the king could consider this sword in exchange for the nail file.The king agrees to the exchange and Aslan rewards you for a job well done."
+
+    button2.textContent = "Congratulations."
+    button2.addEventListener('click',loadStartStage);
+
+    button3.textContent = "Start Over."
+    button3.addEventListener('click',loadStartStage);
 }
